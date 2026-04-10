7876928eca506693fd76f1a878026fde8910d533
@korsakov_v_e <korsacovvova@yandex.ru>
2026-04-10 19:45:45 +0300

    k

--- GM.py
+++ GM.py
@@ -57,3 +57,90 @@ class GroupManager:
         self.gx = self.gs * self.G
     
 
+class Iot:
+
+    def __init__(self, G, p, si, BIi2, M):
+        self.G = G
+        self.p = p
+        self.si = si        # приватный ключ
+        self.BIi2 = BIi2    # анонимный ID
+        self.M = M          # произведение m_i
+
+        self.theta = None
+        self.sigma = None
+
+    def _hash(self, message: bytes):
+        return int.from_bytes(hashlib.sha256(message).digest(), 'big') % self.p
+
+    def GeneratePartSignature(self, message: bytes):
+        # 1. γi
+        gamma_i = secrets.randbelow(self.p)
+
+        # 2. θi = γi * G
+        self.theta = gamma_i * self.G
+        x_theta = self.theta.x
+
+        # 3. μ = h(m)
+        mu = self._hash(message)
+
+        # 4. Ji = BIi2 * M
+        Ji = (self.BIi2 * self.M) % self.p
+
+        # 5. σi = γi * xθi − μ * si * Ji mod p
+        self.sigma = (gamma_i * x_theta - mu * self.si * Ji) % self.p
+
+    def SendPartSignature(self):
+        if self.theta is None or self.sigma is None:
+            raise ValueError("Call GeneratePartSignature() first")
+
+        return {
+            "theta": self.theta,
+            "sigma": self.sigma,
+            "BIi2": self.BIi2,
+            "BIi2_encrypted": self._encrypt_identity()
+        }
+
+    # псевдо-шифрование (заглушка для статьи)
+    def _encrypt_identity(self):
+        rnd = secrets.token_bytes(16)
+        data = rnd + self.BIi2.to_bytes(32, 'big')
+        return hashlib.sha256(data).hexdigest()
+
+
+def test():
+    print("=== INIT GM ===")
+    gm = GroupManager(n=5, t=3)
+    gm.GenerateElepticCurve()
+    gm.GenerateGMKeys()
+    gm.GenerateGroupKeys()
+
+    print("p:", gm.p)
+    print("M:", gm.M)
+
+    # создаём IoT участника
+    si = random.randint(1, gm.p)
+    BIi2 = random.randint(1, gm.p)
+
+    IoT = Iot(
+        G=gm.G,
+        p=gm.p,
+        si=si,
+        BIi2=BIi2,
+        M=gm.M
+    )
+
+    message = b"Hello IoT Threshold Signature"
+
+    print("\n=== GENERATE PARTIAL SIGNATURE ===")
+    IoT.GeneratePartSignature(message)
+
+    sig = IoT.SendPartSignature()
+
+    print("Theta:", sig["theta"])
+    print("Sigma:", sig["sigma"])
+    print("BIi2:", sig["BIi2"])
+    print("Encrypted BIi2:", sig["BIi2_encrypted"])
+
+
+if __name__ == "__main__":
+    test()
