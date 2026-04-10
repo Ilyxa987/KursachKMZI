ab8a349740a440ba2c463e0717989f35d668c31e
Ilyxa987 <eprigprogov228@gmail.com>
2026-04-10 20:11:30 +0300

    TSG

--- TSG.py
+++ TSG.py
@@ -5,6 +5,8 @@ class TSG:
     SKtsg: int # Закрытый ключ TSG
     Ntsg: int # Модуль RSA
     SL: list # Список подписей
+    private_key: RSA.RsaKey
+    public_key: RSA.RsaKey
 
     def __init__(self):
         private_key = RSA.generate(2048)
@@ -12,3 +14,10 @@ class TSG:
         self.Ntsg = public_key.n
         self.PKtsg = public_key.e
         self.SKtsg = private_key.d
+
+    def getPK(self):
+        return self.PKtsg
+    
+    def DecryptAnonIdentificator(self, CipherBI2: int):
+        BI2 = pow(CipherBI2, self.SKtsg, self.Ntsg)
+        return BI2
